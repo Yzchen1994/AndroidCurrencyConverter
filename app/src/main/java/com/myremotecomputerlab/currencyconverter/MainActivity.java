de5cd527c84c935fc499b4bee9ac69c7bb9edeb2
Yzchen1994 <Yzchen1994@Hotmail.com>
2016-04-01 22:44:10 -0400

    Completed the basic part of conversion.

--- app/src/main/java/com/myremotecomputerlab/currencyconverter/MainActivity.java
+++ app/src/main/java/com/myremotecomputerlab/currencyconverter/MainActivity.java
@@ -1,31 +1,45 @@
 package com.myremotecomputerlab.currencyconverter;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+        EditText dollarField = (EditText) findViewById(R.id.dollarField);
+        Double dollarAmt= Double.parseDouble(dollarField.getText().toString());
+        Double resultAmt=dollarAmt*6.48;
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(resultAmt.toString());
+
+        // Hide keyboard
+        //setupUI(findViewById(R.id.dollarField));
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
     @Override
